31a0a36b73ae180550112b268f889b5e5aa39cb2
Faye <fayebheevayne@gmail.com>
2022-03-15 18:12:17 +0800

    added sql commands

---
+++ discussion.sql
@@ -0,0 +1,9 @@
+-- sql commands - use uppercase for best practice
+-- List down the databases inside the DBMS
+SHOW DATABASES;
+
+-- create a database
+CREATE DATABASE music_db;
+
+-- delete a database
+DROP DATABASE music_db;
