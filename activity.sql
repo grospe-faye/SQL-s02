d812937b812a2e7a769c05b2968c6c129aa2f403
Faye <fayebheevayne@gmail.com>
2022-03-15 21:05:40 +0800

    added activity code

--- activity.sql
+++ activity.sql
@@ -6,6 +6,32 @@ CREATE TABLE students (
     PRIMARY KEY (id)  
 );
 
+CREATE TABLE student_courses ( 
+	id INT NOT NULL AUTO_INCREMENT, 
+	course_id INT NOT NULL,
+    student_id INT NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT fk_student_courses_course_id 
+    	FOREIGN KEY (course_id) REFERENCES courses(id)
+    	ON UPDATE CASCADE
+    	ON DELETE RESTRICT,
+    CONSTRAINT fk_student_courses_student_id
+    	FOREIGN KEY (student_id) REFERENCES students(id)
+    	ON UPDATE CASCADE
+    	ON DELETE RESTRICT  
+);
+
+CREATE TABLE courses ( 
+	id INT NOT NULL AUTO_INCREMENT, 
+	course_name VARCHAR(50) NOT NULL,
+    teacher_id INT NOT NULL,
+	PRIMARY KEY (id),
+    CONSTRAINT fk_courses_teacher_id 
+    	FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+    	ON UPDATE CASCADE
+    	ON DELETE RESTRICT
+);
+
 CREATE TABLE teachers (
 	id INT NOT NULL AUTO_INCREMENT,
     teacher_name VARCHAR(50) NOT NULL,
